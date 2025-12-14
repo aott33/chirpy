17130d7b8d02b6ac57a22eefbb17cae611d371a5
aott33 <ott.andrew33@gmail.com>
2025-12-13 22:40:38 -0800

    add simple customer handler

--- main.go
+++ main.go
@@ -2,6 +2,20 @@ package main
 
 import "net/http"
 
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
+
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	mux.HandleFunc("/healthz", healthHandler)
+
+	server := &http.Server{Addr: ":8080", Handler: mux}
+
+	server.ListenAndServe()
 }
