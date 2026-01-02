a6feff4728270fe1316fce9097c63547f2442813
aott33 <ott.andrew33@gmail.com>
2026-01-01 21:37:15 -0800

    implement validate chirp length functionality

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"sync/atomic"
-	"fmt"
 )
 
 type apiConfig struct {
@@ -41,6 +42,68 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK\n"))
 }
 
+func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body	string `json:"body"`
+	}
+
+	type returnError struct {
+		Error	string `json:"error"`
+	}
+
+	type returnValid struct {
+		Valid	bool `json:"valid"`
+	}
+	
+	w.Header().Set("Content-Type", "application/json")
+
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+
+	respBodyError := returnError{}
+	
+	respBodyValid := returnValid{}
+
+	err := decoder.Decode(&params)
+
+	if err != nil {	
+		respBodyError.Error = fmt.Sprintf("Something went wrong: %s", err)
+		dat, err := json.Marshal(respBodyError)
+		if err != nil {
+			fmt.Printf("Something went wrong: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}	
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(dat)
+		return
+	}
+
+	if len(params.Body) > 400 {
+		respBodyError.Error = "Chirp is too long"
+		dat, err := json.Marshal(respBodyError)
+		if err != nil {
+			fmt.Printf("Something went wrong: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(dat)
+		return
+	}
+
+	respBodyValid.Valid = true
+	dat, err := json.Marshal(respBodyValid)
+	if err != nil {
+		fmt.Printf("Something went wrong: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(200)
+	w.Write(dat)
+}
+
 
 func main() {
 	mux := http.NewServeMux()
@@ -54,6 +117,8 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", healthHandler)
 
 	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
+
+	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
 
 	mux.HandleFunc("POST /admin/reset", apiCfg.resetHanlder)
 
